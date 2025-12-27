2a4ef22d8a1ea545b2c0e9e41b40b556b6113dc8
Limax <limax.workspace@gmail.com>
2025-12-27 22:17:31 +0100

    update: decentralize database processes

---
+++ methods.py
@@ -0,0 +1,56 @@
+from db import cursor, connection
+
+# Person Methods
+
+def getPersonById(person_id):
+    cursor.execute("""SELECT * FROM person WHERE id = ?""", (person_id, ))
+    return cursor.fetchone()
+
+def getAllPersons():
+    cursor.execute("""SELECT * from person""")
+    return cursor.fetchall()
+
+# Chore Methods
+
+def getChoresOfPerson(person_id):
+    cursor.execute("""SELECT * FROM chore WHERE worker_id = ?""", (person_id, ))
+    return cursor.fetchall()
+
+def getDueChoresOfPerson(person_id):
+    cursor.execute("""SELECT * FROM chore WHERE worker_id = ? AND (last_done IS NULL OR last_done + interval*86400 <= CAST(strftime('%s', 'now') AS INTEGER))""", (person_id, ))
+    return cursor.fetchall()
+
+def markChoreDone(chore_id):
+    cursor.execute("""UPDATE chore SET last_done = CAST(strftime('%s', 'now') AS INTEGER) WHERE id = ?""", (chore_id, ))
+    connection.commit()
+
+def addChore(name, interval, worker_id):
+    cursor.execute("""INSERT INTO chore (name, interval, worker_id) VALUES (?, ?, ?)""", (name, interval, worker_id))
+    connection.commit()
+
+def removeChore(chore_id):
+    cursor.execute("""DELETE FROM chore WHERE id = ?""", (chore_id,))
+    connection.commit()
+
+
+# Plant Methods
+
+def getPlantsOfPerson(person_id):
+    cursor.execute("""SELECT * FROM plant WHERE owner_id = ?""", (person_id, ))
+    return cursor.fetchall()
+
+def getDuePlantsOfPerson(person_id):
+    cursor.execute("""SELECT * FROM plant WHERE owner_id = ? AND (last_pour IS NULL OR last_pour + interval*86400 <= CAST(strftime('%s', 'now') AS INTEGER))""", (person_id, ))
+    return cursor.fetchall()
+
+def markPlantWatered(plant_id):
+    cursor.execute("""UPDATE plant SET last_pour = CAST(strftime('%s', 'now') AS INTEGER) WHERE id = ?""", (plant_id, ))
+    connection.commit()
+
+def addPlant(name, interval, image, owner_id):
+    cursor.execute("""INSERT INTO plant (name, interval, image, owner_id) VALUES (?, ?, ?, ?)""", (name, interval, image, owner_id))
+    connection.commit()
+
+def removePlant(plant_id):
+    cursor.execute("""DELETE FROM plant WHERE id = ?""", (plant_id,))
+    connection.commit()
